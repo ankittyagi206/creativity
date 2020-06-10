63869b8bb86b2ed5baa147fb0c0c79130f45f8c8
ankit206 <tyagiankit370@gmail.com>
2020-06-10 17:29:34 +0530

    initial commit

---
+++ src/Avatar.js
@@ -0,0 +1,17 @@
+import React from 'react' ;
+import ReactDOM from 'react-dom';
+import './Avatar.css';
+
+const Avatar = () =>{
+return(<>
+	<img className="avatarstyle" src="//joeschmoe.io/api/v1/jess" alt="image"/>
+	<h1>Ankit Tyagi</h1>
+	<p> Web Developer</p>
+
+
+	</>)
+
+
+}
+
+export default Avatar;
